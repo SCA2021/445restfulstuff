82d7a835b80d238e47a29665a647c063ac8774a6
Fred Flintstone <fflintstone@slate.com>
2023-03-22 10:12:23 -0500

    message

--- Express/server.js
+++ Express/server.js
@@ -3,11 +3,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.post('/api/character', (req, res) =>{
         /// todo: mongo stuff
         res.writeHead(200);
         res.end(`{data: "Hello from database"}`);
 });
+
+
+app.get('/api/character/:tag', (req, res) =>{
+  const gamerTag = req.params.tag;
+  res.writeHead(200);
+  console.log(`The client sent us: ${gamerTag} as the gamer tag.`)
+  res.end(`{"characterTag": "${gamerTag}"}`);
+});
+
 
 
 app.use(express.static("public"));
